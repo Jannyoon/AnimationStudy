08983dc94ca54c5af432a23364fb5a29c65eb152
Jannyoon <5124193@naver.com>
2024-08-13 01:17:04 +0900

    (ADD)keyframes

---
+++ src/study1/Study1.jsx
@@ -0,0 +1,11 @@
+import styles from './study1.module.css'
+import React from 'react';
+
+export default function Study1() {
+  return (<>
+      <div className={styles.square}></div>
+      <div className={styles.testText}>text</div>
+    </>
+  );
+}
+
